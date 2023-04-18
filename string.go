a9537f2e5f97710b0433f88acec6b6ad56e4c176
alwani achmad <alwaniachmad7031999aa@gmail.com>
2023-04-18 13:32:20 +0700

    Type data string

---
+++ string.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Alwani")
+	fmt.Println("♡")
+	fmt.Println("Nurul")
+
+	fmt.Println("-------")
+
+	fmt.Println(len("Alwani"))
+	fmt.Println("♡"[0])
+	fmt.Println("Nurul"[1])
+
+}
