e9079fdb32eb46304e47ed2dca092ccf2e4778ff
alwani achmad <alwaniachmad7031999aa@gmail.com>
2023-04-22 19:00:41 +0700

    comparation-operator

---
+++ comparation-operator.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name1 = "alwani"
+	var name2 = "alwani"
+
+	var name_a = "Alwani"
+	var name_b = "Nurul"
+
+	var angka_1 = 100
+	var angka_2 = 150
+
+	var result bool = name1 == name2
+	var resultName bool = name_a > name_b
+	var resultAngka bool = angka_1 < angka_2
+
+	fmt.Println(result)
+	fmt.Println(resultName)
+	fmt.Println(resultAngka)
+
+}
