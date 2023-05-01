5a0c25699e878339c18cea673f05221f2435622d
alwani achmad <alwaniachmad7031999aa@gmail.com>
2023-05-01 14:58:44 +0700

    Array

---
+++ array.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var names [3]string
+	names[0] = "alwani"
+	names[1] = "nurul"
+	names[2] = "kharomah"
+
+	fmt.Println(names[0])
+	fmt.Println(names[1])
+	fmt.Println(names[2])
+
+	var values = [3]int{
+		10,
+		20,
+		30,
+	}
+
+	fmt.Println("---------")
+	fmt.Println(values)
+	fmt.Println("---------")
+
+	fmt.Println(values[0])
+	fmt.Println(values[1])
+	fmt.Println(values[2])
+
+	fmt.Println("---------")
+	fmt.Println("---------")
+
+	fmt.Println(len(names)) /*jumlah length*/
+
+	var lagi [10]int
+	fmt.Println(len(lagi)) /*jumlah length*/
+
+}
